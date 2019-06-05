981b47e45b13892589997896e2ca74997a88230b
Jack Colvin <jcolvin@uoguelph.ca>
2019-06-05 02:32:09 -0400

    Transitioning to a frame-based design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,31 @@
 import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo/seo"
+import Tao from "../components/tao/tao"
+import Intro from "../components/intro/intro"
+
 
 const IndexPage = () =>(
     <Layout>
         <SEO title="Jack Colvin Developer"></SEO>
-        <div>
-            <h2>I'm Jack Colvin.</h2>
-            <h3>A Full Stack Developer.</h3>
+        {/* First Pane (Tao) */}
+        <div style={{
+            display: "flex",
+            flexDirection: "rows"
+        }}> <Intro />
+        </div>
+        {/* First Pane (Tao) */}
+        <div style={{
+            display:"flex",
+            flexDirection:"rows"
+        }}>
+            <Tao/>
+        </div>
+        {/* First Pane (Tao) */}
+        <div style={{
+            display: "flex",
+            flexDirection: "rows"
+        }}> <p>Jack Colvin</p>
         </div>
     </Layout>  
 )
